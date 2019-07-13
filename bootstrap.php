6e93b17821e980b02d0be278b268020e69a6588f
cdanh-aptech <cdanha18037@cusc.ctu.edu.vn>
2019-07-14 01:10:47 +0700

    BS Bai tap 8

--- bootstrap.php
+++ bootstrap.php
@@ -2,8 +2,14 @@
 // Load các thư viện (packages) do Composer quản lý vào chương trình
 require_once __DIR__.'/vendor/autoload.php';
 
+// Start session
+session_start();
+
 // Chỉ định thư mục `templates` (nơi Twig sẽ biên dịch cú pháp Twig thành các đoạn code PHP)
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 
  // Khởi tạo Twig
 $twig = new Twig_Environment($loader);
+
+// Tạo biến global để có thể sử dụng trong tất cả các view được render bởi TWIG
+$twig->addGlobal('session', $_SESSION);
